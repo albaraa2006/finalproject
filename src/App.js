21fe5dd5b69488b0aabaf43981909427456ba41f
albaraa2006 <45728144+albaraa2006@users.noreply.github.com>
2020-06-09 09:48:32 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,174 @@
-import React from 'react';
-import Navigation from './components/Navigation/Navigation';
+import React, { Component } from 'react';
 import './App.css';
-function App() {
-  return (
-    <div className="App">
-     <Navigation />
-   
-    </div>
-  );
+import Navigation from './components/Navigation/Navigation'
+import Logo from './components/Logo'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank'
+import Register from './components/Register/Register'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
+import Particles from 'react-particles-js';
+import Signin from './components/Signin/Signin'
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value:50,
+      density: {
+        enable: true,
+        value_area:300
+      }
+    }
+  },
+  interactivity:{
+    detect_on:"window",
+    events:{
+      onhover:{
+        enable:true,
+        mode:"repulse"}
+      } 
+  }
+}
+
+const initialState = {
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin',
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
+}
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin',
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
+    }
+  }
+
+  // componentDidMount() {
+  //   fetch('https://fierce-anchorage-16152.herokuapp.com/')
+  //     .then(response => response.json())
+  //     .then(console.log)
+  // }
+
+  loadUser = (data) => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+      }
+    })
+  }
+
+  calculateFaceLocation = (data) => {
+    const boxedFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: boxedFace.left_col * width,
+      topRow: boxedFace.top_row * height,
+      rightCol: width - (boxedFace.right_col*width),
+      bottomRow: height - (boxedFace.bottom_row*height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box)
+    this.setState({box:box})
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value)
+    this.setState({input: event.target.value})
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.setState({imageUrl: this.state.input})
+    fetch('https://fierce-anchorage-16152.herokuapp.com/imageurl', {
+          method:'post',
+          headers: {'Content-Type':'application/json'},
+          body: JSON.stringify({
+              input: this.state.input
+          })
+      })
+      .then(response => response.json())
+    .then(response => {
+      if(response){
+        fetch('https://fierce-anchorage-16152.herokuapp.com/image', {
+          method:'put',
+          headers: {'Content-Type':'application/json'},
+          body: JSON.stringify({
+              id: this.state.user.id
+          })
+      })
+        .then(response => response.json())
+        .then(count => {
+          console.log(count)
+          this.setState(Object.assign(this.state.user, {entries:count}))
+        })
+    }
+      this.displayFaceBox(this.calculateFaceLocation(response))
+      })
+    .catch(err => console.log(err))
+  }
+
+  routeChange = (route) => {
+    if(route === 'signin'){
+      this.setState(initialState)
+    } else if (route ==='home'){
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route:route})
+  }
+
+
+ 
+  render() {
+    const {isSignedIn,imageUrl,route,box} = this.state;
+    return (
+      <div className="App">
+        <Particles className='particles'
+          params={particlesOptions}
+        />
+        <Navigation isSignedIn={isSignedIn} routeChange={this.routeChange} />
+        {route === 'home' ?
+          <div>
+            <Logo />
+            <Rank name = {this.state.user.name} entries = {this.state.user.entries}/>
+            <ImageLinkForm inputChange={this.onInputChange} onSubmit={this.onSubmit} />
+            <FaceRecognition box={box} imageUrl={imageUrl} />
+          </div>
+          : (route === 'signin' ?
+            <Signin loadUser = {this.loadUser} routeChange={this.routeChange} /> :
+            <Register loadUser = {this.loadUser} routeChange={this.routeChange} />
+          )
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
